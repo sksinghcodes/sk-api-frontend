3eb53a83ce8e0b92ec163821003138e3d5cd0ef1
Surajkant Singh <sksinghcodes@gmail.com>
2023-03-10 00:11:37 +0530

    added page title functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
 import './App.css';
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 
 function App() {
+
+	const routeTitles = [
+		{
+			url: '/authentication/sign-in',
+			title: 'Sign In',
+		},
+		{
+			url: '/authentication/sign-up',
+			title: 'Sign Up',
+		},
+		{
+			url: '/authentication/verify-profile',
+			title: 'Verify Profile',
+		},
+		{
+			url: '/authentication/reset-password',
+			title: 'Reset Password',
+		},
+		{
+			url: '/data-sources',
+			title: 'Data Sources',
+		},
+		{
+			url: '/data',
+			title: 'Data',
+		},
+		{
+			url: '/',
+			title: 'SK API',
+		},
+	]
+	const { pathname } = useLocation();
+
+	for (let { url, title } of routeTitles){
+		if(pathname.startsWith(url)){
+			document.title = title + ' | SK API';
+			break;
+		}
+	}
+
 	return (
 		<div className="App">
 			<Outlet />
